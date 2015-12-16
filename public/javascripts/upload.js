cf883b0232e4a09a2f74b6fa24d592e7bc1795c7
Diana Shkolnikov <dianashk@gmail.com>
2015-12-15 22:15:32 -0500

    invalid input handling

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -1,4 +1,39 @@
 function submitForm() {
+  // first check if all data was provided
+  // highlight missing stuff in red
+  var email = document.getElementById('email').value;
+  var file = document.getElementById('file').files[0];
+  var confirm = document.getElementById('confirmNice').checked;
+
+  console.log(email, file, confirm);
+
+  document.getElementById('email').style.border = "1px solid darkgray";
+  document.getElementById('inputfileLabel').style.border = "0px";
+  document.getElementById('confirmNiceLabel').style.color = "black";
+
+  var badData = false;
+
+  if (!email || email === '' || email.indexOf('@') === -1) {
+    badData = true;
+    document.getElementById('email').style.borderColor = "red";
+    document.getElementById('email').style.borderWidth = "5px";
+    console.log('invalid email');
+  }
+  if (!file || file.type !== 'text/csv') {
+    badData = true;
+    document.getElementById('inputfileLabel').style.border = "5px solid red";
+    console.log('bad file type');
+  }
+  if (!confirm) {
+    badData = true;
+    document.getElementById('confirmNiceLabel').style.color = "red";
+    console.log('not nice');
+  }
+
+  if (badData) {
+    return;
+  }
+
   var form = document.forms.namedItem("uploadForm");
   var formData   = new FormData(form);
 
